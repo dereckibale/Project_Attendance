33db980ab49e3fcd9c7b84c207bb91079b7015d7
dereckibale <ibaledereck65@gmail.com>
2024-02-16 14:53:23 +0800

    Changed from "b" to "B"

--- activate_virtual_env.bat
+++ activate_virtual_env.bat
@@ -1,4 +1,4 @@
 :: This command attempts to activate a Python virtual environment by trying different possible paths to the 'activate' script.
 :: It tries the 'venv', 'virtual', and 'Virtual' directories within the 'backend' directory one by one until one succeeds.
 
-call backend\venv\Scripts\activate || call backend\virtual\Scripts\activate || call backend\Virtual\Scripts\activate
+call Backend\venv\Scripts\activate || call Backend\virtual\Scripts\activate || call Backend\Virtual\Scripts\activate
